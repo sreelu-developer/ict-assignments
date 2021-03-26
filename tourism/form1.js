516b6f63023f7ed8a9963cd6a6c9a6d08f183cae
sreelu-developer <72683041+sreelu-developer@users.noreply.github.com>
2021-03-26 23:35:21 +0530

    Add files via upload

---
+++ tourism/form1.js
@@ -0,0 +1,79 @@
+function validate()
+{
+	var v1=emailvalidate();
+	var v2=passvalidate();
+	if(v1&&v2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+function emailvalidate()
+{
+	let email =document.getElementById("mail");
+	let error =document.getElementById("error");
+	let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\.-]+).([a-z]{2,3}).(.[a-z]{2,3})?$/;
+	if(email.value=="")
+	{
+		error.innerHTML="please enter something!";
+		error.style.color="red";
+		return false;
+	}
+	else
+	{
+    if(regexp.test(mail.value))
+	{
+		error.innerHTML="Valid";
+		error.style.color="green";
+		return true;
+	}
+	else
+	{
+		error.innerHTML="Invalid";
+		error.style.color="red";
+		return false;
+	}
+	
+		
+	}
+}
+function passvalidate()
+{
+
+	let error1 =document.getElementById("error1");
+	let pass =document.getElementById("pass");
+	
+	let medium=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
+	let strong=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{14,}$/;
+	if(pass.value == "")
+	{ 
+	   error1.innerHTML="please enter something!";
+		error1.style.color="red";
+	   return false;
+	}
+	else
+	{
+	if((strong.test(pass.value)))
+	{
+		  error1.innerHTML="Strong!";
+		error1.style.color="Green";
+		return true;
+	}
+	else if((medium.test(pass.value)))
+	{
+		  error1.innerHTML="medium!";
+		error1.style.color="orange";
+		return true;
+	}
+	
+	else
+	{
+		error1.innerHTML="Week and Invalid";
+		error1.style.color="red";
+		return false;
+	}
+	}
+}
